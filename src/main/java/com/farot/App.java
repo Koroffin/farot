deccdac660dc4723ab2a48e2129e3cefca68773c
Koroffin <n.koroffin@gmail.com>
2017-04-10 01:52:05 +0300

    enable cors

--- src/main/java/com/farot/App.java
+++ src/main/java/com/farot/App.java
@@ -16,9 +16,36 @@ public class App
 {
   private static Gson gson = new Gson();
 
+  private static void enableCORS() {
+
+    options("/*",
+      (request, response) -> {
+
+        String accessControlRequestHeaders = request
+                .headers("Access-Control-Request-Headers");
+        if (accessControlRequestHeaders != null) {
+            response.header("Access-Control-Allow-Headers",
+                    accessControlRequestHeaders);
+        }
+
+        String accessControlRequestMethod = request
+                .headers("Access-Control-Request-Method");
+        if (accessControlRequestMethod != null) {
+            response.header("Access-Control-Allow-Methods",
+                    accessControlRequestMethod);
+        }
+
+        return "OK";
+      });
+
+    before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+  }
+
   public static void main( String[] args )
   {
     staticFiles.location("/public");
+
+    enableCORS();
 
     post(Path.Web.api.Account.DEFAULT, (req, res) -> { 
       return AccountController.createAccount(req, res); 
