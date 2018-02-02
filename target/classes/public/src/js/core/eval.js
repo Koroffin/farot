b445b81fbd48c9e2d0b3eb00a85cb4a26bc8540c
Nikita Korovin <n.koroffin@gmail.com>
2018-02-02 22:33:56 +0800

    .eval float support

--- target/classes/public/src/js/core/eval.js
+++ target/classes/public/src/js/core/eval.js
@@ -11,6 +11,7 @@
         NUMBER_TYPE: 'number',
         VARIABLE_TYPE: 'variable',
         STRING_TYPE: 'string',
+        FLOAT_TYPE: 'float',
 
         ERROR_STATUS: 'error',
         OK_STATUS: 'ok',
@@ -19,8 +20,11 @@
         STRING_MATCH: '\'',
         STRING_MATCH_DOUBLE: '"',
 
+        DOT: '.',
+
         _addSymbol: function (symbol) {
             this.readedSubstr += symbol;
+            return this;
         },
         addSymbol: function (symbol) {
             var res = this.OK_STATUS;
@@ -28,6 +32,7 @@
             try {
                 var isChar = /[a-z]/i.test(symbol);
                 var isNumber = /[0-9]/i.test(symbol);
+                var isWhiteSpace = /\s/.test(symbol);
 
                 // Catching typical errors
                 if (isChar && !this.canAddChar()) {
@@ -38,25 +43,33 @@
                 }
 
                 // Checking other symbols
-                var isWhiteSpace = /\s/.test(symbol);
                 if (this.isString()) {
-                    if (this.isStringMatch(symbol)) {
+                    if (this.isStringMatch(symbol) && (this.stringStartedSymbol === symbol)) {
                         res = this.END_STATUS;
                     } else {
                         this._addSymbol(symbol);
                     }                    
                 } else if (isWhiteSpace) {
-                    // ignore it
-                } else  if (this.isUndefined()) {
+                    if (this.isUndefined()) {
+                        // ignore it
+                    } else {
+                        res = this.END_STATUS;
+                    }                    
+                } else if (this.isUndefined()) {
                     // need initialize type
                     if (isNumber) {
-                        this.setType(this.NUMBER_TYPE);
-                        this._addSymbol(symbol);
+                        this.setType(this.NUMBER_TYPE)
+                            ._addSymbol(symbol);
                     } else if (isChar) {
-                        this.setType(this.VARIABLE_TYPE);
-                        this._addSymbol(symbol);
+                        this.setType(this.VARIABLE_TYPE)
+                            ._addSymbol(symbol);
                     } else if (this.isStringMatch(symbol)) {
-                        this.setType(this.STRING_TYPE);
+                        this.setType(this.STRING_TYPE)
+                            .stringStartedSymbol = symbol;
+                    } else if (this.isDot(symbol)) {
+                        this.setType(this.FLOAT_TYPE)
+                            ._addSymbol('0')
+                            ._addSymbol(symbol);
                     } else {
                         throw new Error('Can not add start a word with ' + symbol);
                     }
@@ -64,6 +77,9 @@
                     this._addSymbol(symbol);
                 } else if (isNumber && this.canAddNumber()) {
                     this._addSymbol(symbol);
+                } else if (this.isDot(symbol) && this.isNumber()) {
+                    this.setType(this.FLOAT_TYPE)
+                        ._addSymbol(symbol);
                 } else {
                     throw new Error('Can not add symbol ' + symbol + ' to word ' + this.readedSubstr);
                 }
@@ -87,19 +103,32 @@
             if (this.isVariable()) {
                 return parent[this.readedSubstr];
             }
+            if (this.isFloat()) {
+                return parseFloat(this.readedSubstr, 10);
+            }
         },
         setType: function (type) {
             this.type = type;
+            return this;
         },
+        last: function () {
+            return F.last(this.readedSubstr);
+        },
+
         canAddNumber: function () {
-            return (this.type === this.UNDEFINED_TYPE) || (this.type === this.NUMBER_TYPE) || (this.type === this.STRING_TYPE);
+            return this.isUndefined() || this.isNumber() || this.isString() || this.isFloat();
         },
         canAddChar: function () {
-            return (this.type === this.UNDEFINED_TYPE) || (this.type === this.VARIABLE_TYPE) || (this.type === this.STRING_TYPE);
+            return this.isUndefined() || this.isVariable() || this.isString();
         },
+
         isStringMatch: function (symbol) {
             return (symbol === this.STRING_MATCH) || (symbol === this.STRING_MATCH_DOUBLE);
         },
+        isDot: function (symbol) {
+            return (symbol === this.DOT);
+        },
+
         isString: function () {
             return this.type === this.STRING_TYPE;
         },
@@ -111,6 +140,9 @@
         },
         isVariable: function () {
             return this.type === this.VARIABLE_TYPE;
+        },
+        isFloat: function () {
+            return this.type === this.FLOAT_TYPE;
         }
     };
 
