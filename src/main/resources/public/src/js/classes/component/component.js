e884ff0770bf87c512b28ee3678af4e2f540f75a
Nikita Korovin <n.koroffin@gmail.com>
2018-02-12 23:25:17 +0800

    extend's example + minor refactoring

---
+++ src/main/resources/public/src/js/classes/component/component.js
@@ -0,0 +1,61 @@
+F.define(
+    './componentRender',
+    'core/attr',
+    'core/assign',
+function (componentRender) {
+    'use strict';
+    function Component (options) {
+        this.options = options;
+        return this;
+    }
+    Component.prototype = F.assign({
+        render: function (callback) {
+            var me      = this,
+                options = this.options;
+
+            this.renderComponents(function (element) {
+                me.element = element;
+
+                // DOM-свойства элемента
+                if (F.isDefined(options.props)) {
+                    for (var i = 0, l = options.props.length; i < l; i++) {
+                        F.setAttr(this.element, options.props[i].name, options.props[i].value);
+                    }
+                }
+
+                me.initHandlers();
+
+                callback(element);
+            });
+            return this;
+        },
+        initHandlers: function () {
+            this.handlers = [ ];
+            if (F.isDefined(options.handlers)) {
+                for (var i = 0, l = options.handlers.length; i < l; i++) {
+                    this.handlers.push(
+                        new options.handlers[i](this.element)
+                    );
+                }
+            }
+        },
+        destroyHandlers: function () {
+            for (var i = 0, l = this.handlers.length; i < l; i++) {
+                this.handlers[i].destroy();
+            }
+            return this;
+        },
+        afterDestroy: function () {
+            if (F.isFunction(this.options.afterDestroy)) {
+                this.options.afterDestroy();
+            }
+            return this;
+        },
+        destroy: function () {
+            this
+                .destroyHandlers()
+                .afterDestroy();
+        }
+    }, componentRender);
+    return Component;
+});
